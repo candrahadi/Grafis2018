e1aa16741dbfae66584a9d9e88d84c1dcb7b5ea2
jangkungari <39754331+jangkungari@users.noreply.github.com>
2018-06-05 23:18:22 +0700

    Update Objek.java

--- Objek.java
+++ Objek.java
@@ -1,1 +1,35 @@
+package org.yourorghere;
+import javax.media.opengl.GL;
+import javax.media.opengl.glu.GLU;
+import javax.media.opengl.glu.GLUquadric;
+public class Objek {
+    static float angle = 0f;
+    static float direction=1f;
+    static void Sun(GL gl)
+    {
+    float BODY_RADIUS=1f;
 
+    int SLICES=50;
+    int STACKS=50;
+    GLU glu=new GLU();
+    GLUquadric q=glu.gluNewQuadric();
+
+    glu.gluSphere(q, BODY_RADIUS, SLICES, STACKS);
+//    gl.glShadeModel(GL.GL_SMOOTH);
+    }
+    
+    static void earth(GL gl)
+    {
+    gl.glTranslatef(5f, 0f, 0f);
+    gl.glRotatef(angle, 0, 1, 1);
+    float BODY_RADIUS=0.4f;
+
+    int SLICES=30;
+    int STACKS=30;
+    GLU glu=new GLU();
+    GLUquadric q=glu.gluNewQuadric();
+
+    glu.gluSphere(q, BODY_RADIUS, SLICES, STACKS);
+    angle += direction;
+    }
+}
