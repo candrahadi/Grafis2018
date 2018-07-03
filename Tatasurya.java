3961712c0e22ca6886752470848f5cdd26421d8d
candrahadi <39544859+candrahadi@users.noreply.github.com>
2018-07-03 22:04:21 +0700

    Update Tatasurya.java

--- Tatasurya.java
+++ Tatasurya.java
@@ -9,123 +9,184 @@ import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLCanvas;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.glu.GLU;
+import com.sun.opengl.util.Animator;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.media.opengl.GLCanvas;
+import javax.media.opengl.GLCapabilities;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPopupMenu;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.UIManager;
+import javax.swing.WindowConstants;
 
-public class Pertemuan_4 implements GLEventListener {
-    float angle = 0.0f;
-    float angles = 90.0f;
-    float roll = 0.0f;
-    float rolling = 0.01f;
-    float direction=0.09f;
 
-    public static void main(String[] args) {
-        Frame frame = new Frame("Simple JOGL Application");
-        GLCanvas canvas = new GLCanvas();
-
-        canvas.addGLEventListener(new Pertemuan_4());
-        frame.add(canvas);
-        frame.setSize(1280, 720);
-        final Animator animator = new Animator(canvas);
-        frame.addWindowListener(new WindowAdapter() {
-
-            @Override
-            public void windowClosing(WindowEvent e) {
-                // Run this on another thread than the AWT event queue to
-                // make sure the call to Animator.stop() completes before
-                // exiting
-                new Thread(new Runnable() {
-
-                    public void run() {
-                        animator.stop();
-                        System.exit(0);
-                    }
-                }).start();
-            }
-        });
-        // Center frame
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-        animator.start();
+public class Pertemuan_4 extends JFrame {
+    static {
+    // When using a GLCanvas, we have to set the Popup-Menues to be HeavyWeight,
+    // so they display properly on top of the GLCanvas
+    JPopupMenu.setDefaultLightWeightPopupEnabled(false);
     }
+    private Animator animator;
+    
+    public Pertemuan_4() {
+ initComponents();
+ final GLRenderer glrender = new GLRenderer();
+ KeyListener mylisterner = new KeyListener() {
+ public void keyTyped(KeyEvent e) {
+ System.out.println("typed "+e.getKeyCode());
+ }
+ 
+ public void keyPressed(KeyEvent e) {
+ System.out.println("pressed "+e.getKeyCode());
+ glrender.Key_Pressed(e.getKeyCode());
+ canvas.repaint();
+ }
 
-    public void init(GLAutoDrawable drawable) {
-        GL gl = drawable.getGL();
-        gl.glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
-        gl.glShadeModel(GL.GL_FLAT);
+ public void keyReleased(KeyEvent e) {
+ System.out.println("released "+e.getKeyCode());
+ }
+ };
+ canvas.addGLEventListener(glrender);
 
-        float ambient[] = {1.0f, 1.0f, 1.0f, 1.0f};
-        float difusse[] = {1.0f, 1.0f, 1.0f, 1.0f};
-        float specular[] = {0.2f, 1.0f, 0.2f, 1.0f};
-        float position[] = {20.0f, 30.0f, 20.0f, 0.0f};
+ Timer mytimer = new Timer();
+ TimerTask mytimertask = new TimerTask() {
 
-        gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT, ambient, 0);
-        gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, difusse, 0);
-        gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, position, 0);
-        gl.glMaterialfv(GL.GL_FRONT, GL.GL_SPECULAR,
-                specular, 0);
+ @Override
+ public void run() {
 
-        float[] mambient = {0.1745f, 0.01175f, 0.01175f, 0.55f
-        };
-        float[] mdiffuse = {0.61424f, 0.04136f, 0.04136f, 0.55f
-        };
-        float[] mspecular = {0.727811f, 0.626959f, 0.626959f,
-            0.55f};
-        float mshine = 76.8f;
-        gl.glMaterialfv(GL.GL_FRONT, GL.GL_AMBIENT, mambient, 0);
-        gl.glMaterialfv(GL.GL_FRONT, GL.GL_DIFFUSE, mdiffuse, 0);
-        gl.glMaterialfv(GL.GL_FRONT, GL.GL_SPECULAR, mspecular,
-                0);
-        gl.glMaterialf(GL.GL_FRONT, GL.GL_SHININESS, mshine);
-        gl.glEnable(GL.GL_LIGHTING);
-        gl.glEnable(GL.GL_LIGHT0);
-        gl.glEnable(GL.GL_DEPTH_TEST);
-        gl.glEnable(GL.GL_NORMALIZE);
-        // drawable.addMouseListener(this);
-        // drawable.addMouseMotionListener(this);
-    }
+ //glrender.draw_text(glut, gl);
+ canvas.repaint();
 
-    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
-        GL gl = drawable.getGL();
-        GLU glu = new GLU();
+ //To change body of generated methods, choose Tools | Templates.
+ }
+ };
 
-        if (height <= 0) { // avoid a divide by zero error!
+ mytimer.schedule(mytimertask, 100, 100);
+ addKeyListener(mylisterner);
+ animator = new Animator(canvas);
+ this.setLocationRelativeTo(null);
+ this.setSize(1920,1080);
+ animator = new Animator(canvas);
+ // This is a workaround for the GLCanvas not adjusting its size, when the frame is resized.
+ canvas.setMinimumSize(new Dimension());
 
-            height = 1;
-        }
-        final float h = (float) width / (float) height;
-        gl.glViewport(0, 0, width, height);
-        gl.glMatrixMode(GL.GL_PROJECTION);
-        gl.glLoadIdentity();
-        glu.gluPerspective(45.0f, h, 1.0, 20.0);
-        gl.glMatrixMode(GL.GL_MODELVIEW);
-        gl.glLoadIdentity();
-    }
+ this.addWindowListener(new WindowAdapter() {
+ @Override
+ public void windowClosing(WindowEvent e) {
+ // Run this on another thread than the AWT event queue to
+ // make sure the call to Animator.stop() completes before
+ // exiting
+ new Thread(new Runnable() {
+     
+ public void run() {
+ animator.stop();
+System.exit(0);
+ }
+ }).start();
+ }
+ });
+ }
+    @Override
+ public void setVisible(boolean show){
+ if(!show)
+ animator.stop();
+ super.setVisible(show);
+ if(!show)
+ animator.start();
+ }
+ /** This method is called from within the constructor to
+ * initialize the form.
+ * WARNING: Do NOT modify this code. The content of this method
+is
+ * always regenerated by the Form Editor.
+ */
+ @SuppressWarnings("unchecked")
+ // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+ private void initComponents() {
+ JLabel label = new JLabel();
+ canvas = new GLCanvas(createGLCapabilites());
+ setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+ label.setText("Gunakan Keyboard untuk menggerakkan kamera");
+ GroupLayout layout = new GroupLayout(getContentPane());
+ getContentPane().setLayout(layout);
+ layout.setHorizontalGroup(
+ layout.createParallelGroup(Alignment.LEADING)
+ .addGroup(layout.createSequentialGroup()
+ .addContainerGap()
 
-    public void display(GLAutoDrawable drawable) {
-        GL gl = drawable.getGL();
-        GLU glu = new GLU();
+.addGroup(layout.createParallelGroup(Alignment.LEADING)
+ .addComponent(canvas, GroupLayout.DEFAULT_SIZE,
+380, Short.MAX_VALUE)
+ .addComponent(label))
+ .addContainerGap())
 
-        // Clear the drawing area
-        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
-        gl.glLoadIdentity();
+ );
+ layout.setVerticalGroup(
+         
+ layout.createParallelGroup(Alignment.LEADING)
+ .addGroup(layout.createSequentialGroup()
+ .addContainerGap()
+ .addComponent(label)
+ .addPreferredGap(ComponentPlacement.RELATED)
+ .addComponent(canvas, GroupLayout.DEFAULT_SIZE, 255,
+Short.MAX_VALUE)
+ .addContainerGap())
 
-        gl.glLoadIdentity();
-        gl.glTranslatef(0f, 0.0f, -12.0f);
-        Objek objek = null;
-        objek = new Objek();
-        
-        gl.glTranslatef(0, 0, roll);
-        gl.glRotatef(angles, 1, 0, 0);
-        gl.glRotatef(angle, 0, 0, 1);
-        objek.Sun(gl);
-       
-        gl.glLoadIdentity();
-        gl.glTranslatef(0f, 0f, -12.0f);
-        gl.glRotatef(angle, 0, 1, 0);
-        objek.earth(gl);
+ );
+ pack();
+ }// </editor-fold>//GEN-END:initComponents
+ /**
+ * Called from within initComponents().
+ * hint: to customize the generated code choose 'Customize Code'
+in the contextmenu
+ * of the selected UI Component you wish to cutomize in design
+mode.
+ * @return Returns customized GLCapabilities.
+ */
+ private GLCapabilities createGLCapabilites() {
 
-        gl.glFlush();
-    }
+ GLCapabilities capabilities = new GLCapabilities();
+ capabilities.setHardwareAccelerated(true);
+ // try to enable 2x anti aliasing - should be supported on most hardware
+ capabilities.setNumSamples(2);
+ capabilities.setSampleBuffers(true);
 
-    public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
-    }
+ return capabilities;
+ }
+
+ /**
+ * @param args the command line arguments
+ */
+ public static void main(String args[]) {
+ // Run this in the AWT event thread to prevent deadlocks and race conditions
+ EventQueue.invokeLater(new Runnable() {
+ public void run() {
+ // switch to system l&f for native font rendering etc.
+ try{
+
+UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+ }catch(Exception ex) {
+
+Logger.getLogger(getClass().getName()).log(Level.INFO, "can not enable system look and feel", ex);
+ }
+ Pertemuan_4 frame = new Pertemuan_4();
+ frame.setVisible(true);
+ }
+ });
+ }
+ // Variables declaration - do not modify//GEN-BEGIN:variables
+ private GLCanvas canvas;
+
 }
